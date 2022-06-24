cdeefb71d8c0882a561be1509bee4da384bad2b2
AlexBoronin <acivinel@mail.ru>
2022-06-25 00:12:48 +0300

    Пытаюсь перейти на ветку а не дает, потому что надо коммиттить

--- task_2_4.py
+++ task_2_4.py
@@ -16,14 +16,34 @@
 # Функция должна корректно обрабатывать любой код валюты. Правильность параметра url можно не проверять.
 # Вводить коды валют с клавиатуры (input) необязательно.
 
-
 from requests import get
 
-def currency_rates(url, ):
-    pass
+
+def currency_rates(req, curr):
+    if 4 <= len(curr) < 3 or req.find(curr) == -1 or curr.isalpha() != True:
+        # print('Incorrect currency abbreviation!!!  Try next time, please')
+        return
+    elif curr in req:
+        nom = req[(req.find('Nominal', req.find(curr)) + len('Nominal') + 1):(
+                req.find('Nominal', ((req.find('Nominal', req.find(curr))) + 1)) - 2)]
+        price = round(float((req[(req.find('Value', req.find(curr)) + len('Value') + 1):(
+                    req.find('Value', ((req.find('Value', req.find(curr))) + 1)) - 2)]).replace(',', '.')), 2)
+        out = f'курс {curr} ----> {nom} {curr} = {price} рублей'
+        print(out)
 
 
-from requests import get
-response = get('http://geekbrains.ru')
-print(type(response)) # <class 'requests.models.Response'>
-print(dir(response))
+req = get('http://www.cbr.ru/scripts/XML_daily.asp').text
+print(
+    '== AUD, AZN, GBP, AMD, BYN, BGN, BRL, HUF, HKD, DKK, USD, EUR, INR, KZT, CAD, KGS, CNY ==\n== MDL, NOK, PLN, RON, XDR, SGD, TJS, TRY, TMT, UZS, UAH, CZK, SEK, CHF, ZAR, KRW, JPY ==')
+curr = input('Enter currency abbreviation: ').upper()
+
+currency_rates(req, curr)
+
+# print(req)
+# print(type(req.text))
+# print(dir(req))
+# print(type(req)) # <class 'requests.models.Response'>
+# print(str(req))
+# print(req.encoding)
+# print(req.headers)
+# print(req.elapsed)
