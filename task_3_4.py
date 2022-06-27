d8cd2cd440dea33654978f440a0c10dd91234faf
AlexBoronin <acivinel@mail.ru>
2022-06-27 20:04:56 +0300

    Намудрил в виртуалкой, не могу разобраться с ветками

--- task_3_4.py
+++ task_3_4.py
@@ -31,17 +31,14 @@
 
 from requests import get
 import datetime
-import test as t
+
 
 def currency_rates_advanced(req, curr):
     if 4 <= len(curr) < 3 or req.find(curr) == -1 or curr.isalpha() != True:
-        # print('Incorrect currency abbreviation!!!  Try next time, please')
         return
     elif curr in req:
-        nom = req[(req.find('Nominal', req.find(curr)) + len('Nominal') + 1):(
-                req.find('Nominal', ((req.find('Nominal', req.find(curr))) + 1)) - 2)]
         price = round(float((req[(req.find('Value', req.find(curr)) + len('Value') + 1):(
-                    req.find('Value', ((req.find('Value', req.find(curr))) + 1)) - 2)]).replace(',', '.')), 2)
+                req.find('Value', ((req.find('Value', req.find(curr))) + 1)) - 2)]).replace(',', '.')), 2)
 
     a = req[req.find('"', req.find('Date')) + 1: req.find('"', req.find('Date')) + 11].split('.')
     date_today = datetime.date(int(a[2]), int(a[1]), int(a[0]))
