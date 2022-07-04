ee6a27aeaf9c1c554cd27e42cb4fd75d152fb902
AlexBoronin <acivinel@mail.ru>
2022-07-04 19:57:56 +0300

    Сделано ПЗ №1 Урока 6. Работа с файлами

--- lesson_6/task_1_6.py
+++ lesson_6/task_1_6.py
@@ -22,14 +22,30 @@
 #
 # Файл логов можно загрузить отсюда: https://raw.githubusercontent.com/elastic/examples/master/Common%20Data%20Formats/nginx_logs/nginx_logs
 # В подобных структурированных файлах разделитель полей всегда очевиден.
+import os.path
 
 from requests import get, utils
-from sys import getsizeof
+from os.path import join, isfile
 
-
-ser = get('https://raw.githubusercontent.com/elastic/examples/master/Common%20Data%20Formats/nginx_logs/nginx_logs') #.text
-print(getsizeof(ser))
-encodings = utils.get_encoding_from_headers(ser.headers)
-content = ser.content.decode(encoding=encodings)
-print(content)
-print(getsizeof(content))
+url = get('https://raw.githubusercontent.com/elastic/examples/master/Common%20Data%20Formats/nginx_logs/nginx_logs')
+encodings = utils.get_encoding_from_headers(url.headers)
+content = url.content.decode(encoding=encodings)
+log_path = join('.', 'log_file.txt')
+log_path_new = join('.', 'log_file_new.txt')
+log_file = open(file=log_path, mode='w+', encoding='utf-8')
+log_file_new = open(file=log_path_new, mode="w", encoding='utf-8')
+list_prod = []
+for lines in content:
+    log_file.write(lines)
+log_file.close()
+log_file = open(file=log_path, mode='rt', encoding='utf-8')
+for line in log_file:
+    mac_adr = line[0:line.find('-')-1]
+    serv = line[line.find('"')+1:line.find('/', line.find('"'))]
+    prod = (line[line.find('/', line.find(serv)):line.find('HTTP')-1])
+    # tuple_prod = mac_adr, serv, prod
+    list_prod.append((mac_adr, serv, prod))
+    log_file_new.write(f'{mac_adr} - {serv} - {prod} \n')
+print(list_prod)
+log_file.close()
+log_file_new.close()
