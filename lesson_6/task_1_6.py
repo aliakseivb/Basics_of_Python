e5e4d4a4e63c0b0b2c82d322b5d9670f951bfa19
AlexBoronin <acivinel@mail.ru>
2022-07-05 12:12:02 +0300

    Немного изменен task_1_6. Решение ПЗ 2 есть, но считаю не удалось

--- lesson_6/task_1_6.py
+++ lesson_6/task_1_6.py
@@ -32,7 +32,7 @@ encodings = utils.get_encoding_from_headers(url.headers)
 content = url.content.decode(encoding=encodings)
 log_path = join('.', 'log_file.txt')
 log_path_new = join('.', 'log_file_new.txt')
-log_file = open(file=log_path, mode='w+', encoding='utf-8')
+log_file = open(file=log_path, mode='w', encoding='utf-8')
 log_file_new = open(file=log_path_new, mode="w", encoding='utf-8')
 list_prod = []
 for lines in content:
@@ -41,9 +41,8 @@ log_file.close()
 log_file = open(file=log_path, mode='rt', encoding='utf-8')
 for line in log_file:
     mac_adr = line[0:line.find('-')-1]
-    serv = line[line.find('"')+1:line.find('/', line.find('"'))]
+    serv = line[line.find('"')+1:line.find(' ', line.find('"'))]
     prod = (line[line.find('/', line.find(serv)):line.find('HTTP')-1])
-    # tuple_prod = mac_adr, serv, prod
     list_prod.append((mac_adr, serv, prod))
     log_file_new.write(f'{mac_adr} - {serv} - {prod} \n')
 print(list_prod)
